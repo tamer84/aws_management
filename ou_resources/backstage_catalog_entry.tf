450323b54f595bab2de5faf84b0e743298d1552d
Tamer Husnu <tamer.husnu@crealogix.com>
2024-11-25 08:53:00 +0100

    Clean history for public

--- ou_resources/backstage_catalog_entry.tf
+++ ou_resources/backstage_catalog_entry.tf
@@ -1,9 +1,7 @@
-# comment me back in to generate catalog files for Backstage
+resource "local_file" "backstage_catalog" {
+  filename = "../.backstage/ou/${aws_organizations_organizational_unit.ou.id}.yaml"
 
-# resource "local_file" "backstage_catalog" {
-#   filename = "../.backstage/ou/${aws_organizations_organizational_unit.ou.id}.yaml"
-#
-#   content = templatefile("${path.module}/catalog-entry.template", {
-#     ou = var.ou_name
-#   })
-# }
+  content = templatefile("${path.module}/catalog-entry.template", {
+    ou = var.ou_name
+  })
+}
