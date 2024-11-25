c626d3c6578b268265b02493ae7e927f740e336d
Tamer Husnu <tamer.husnu@crealogix.com>
2024-11-25 09:08:12 +0100

    Clean history for public

--- account_resources/backstage_catalog_entry.tf
+++ account_resources/backstage_catalog_entry.tf
@@ -4,10 +4,11 @@ resource "local_file" "backstage_catalog" {
 
   filename = "../.backstage/accounts/${var.parent_ou_id}-${var.environment}.yaml"
   content = templatefile("${path.module}/catalog-entry.template", {
-    ou_name     = local.ou_name
-    environment = var.environment
-    region      = var.region
-    account_id  = data.external.get_account_id.result.Id
-    owner       = var.sso_email
+    parent_ou_name = local.ou_name
+    parent_ou_id   = var.parent_ou_id
+    environment    = var.environment
+    region         = var.region
+    account_id     = data.external.get_account_id.result.Id
+    owner          = var.sso_email
   })
 }
--- ou_resources/backstage_catalog_entry.tf
+++ ou_resources/backstage_catalog_entry.tf
@@ -2,6 +2,7 @@ resource "local_file" "backstage_catalog" {
   filename = "../.backstage/ou/${aws_organizations_organizational_unit.ou.id}.yaml"
 
   content = templatefile("${path.module}/catalog-entry.template", {
-    ou = var.ou_name
+    ou    = var.ou_name
+    ou_id = aws_organizations_organizational_unit.ou.id
   })
 }
